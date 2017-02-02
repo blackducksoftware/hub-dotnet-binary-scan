352819cd6ca8ea35c85074b7b5c4e65bcd5e93a3
Yev Bronshteyn <yevster@gmail.com>
2017-02-02 00:15:54 -0500

    Adding non-recursive Sha1 computation

--- Scanner/Program.cs
+++ Scanner/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Security.Cryptography;
 using System.Reflection;
+using System.IO;
 
 namespace Scanner
 {
     class Program
     {
+        private static readonly SHA1Cng sha1 = new SHA1Cng();
+
         static void Main(string[] args)
         {
             if (args.Length < 1 || string.IsNullOrWhiteSpace(args[0]))
@@ -21,12 +25,39 @@ namespace Scanner
             Console.WriteLine("Scanning " + target + "...");
             Assembly targetAssembly = Assembly.LoadFile(target);
             Console.WriteLine(targetAssembly.FullName+" "+targetAssembly.GetName().GetPublicKey());
-            foreach (var refAssembly in targetAssembly.GetReferencedAssemblies())
+            //Retrieve the referenced assembly
+            foreach (var refAssemblyName in targetAssembly.GetReferencedAssemblies())
             {
-                Console.WriteLine(refAssembly.FullName + " " + refAssembly.GetPublicKey());
-                
+
+                //Load the assembly to get its path
+                Assembly refAssembly = Assembly.Load(refAssemblyName);
+                string path =refAssembly.Location.ToString();
+
+ 
+                Console.Write(refAssembly.FullName + " " + refAssemblyName.GetPublicKey() + " -> ");
+                Console.Write(path + " [" );
+
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    string sha1 = computeSha1(path);
+                    Console.Write(sha1);
+                }
+                Console.WriteLine("]");
+
+
             }
             Console.ReadKey();
         }
+
+        private static String computeSha1(String path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                byte[] hash = sha1.ComputeHash(fs);
+                return Convert.ToBase64String(hash);
+            }
+        }
     }
+
+  
 }
