c0b0f8cee871be8c8988db9499169d55214b148d
Yev Bronshteyn <yevster@gmail.com>
2017-04-10 02:51:18 -0400

    Adding automated testing (scanning of project)

---
+++ ScannerFullTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScannerFullTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScannerFullTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a50000fd-7a02-46dd-b114-c7ad0d34aab7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ScannerFullTest/ScanTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackduck.Hub
+{
+    [TestClass]
+    public class ScanTest
+    {
+
+        private DirectoryInfo tempProjectDirectory;
+        private AppDomain scanDomain;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            string tempProjectPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            tempProjectDirectory = Directory.CreateDirectory(tempProjectPath);
+            ZipFile.ExtractToDirectory(@"TestApp.zip", tempProjectPath);
+            scanDomain = AppDomain.CreateDomain("Scanner Test Domain");
+
+        }
+
+        [TestMethod]
+        public void TestScan()
+        {
+            string executablePath = Path.Combine(tempProjectDirectory.FullName, "QuackHackAdmin.exe");
+            string outputFilePath = Path.Combine(tempProjectDirectory.FullName, "scanresult.json");
+            scanDomain.ExecuteAssembly("Scanner.exe", new string[] {executablePath, outputFilePath });
+            string generatedJson = File.ReadAllText(outputFilePath);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(generatedJson));
+            
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            AppDomain.Unload(scanDomain);
+            tempProjectDirectory.Delete(true);
+        }
+    }
+}
